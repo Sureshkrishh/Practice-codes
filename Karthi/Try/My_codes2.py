7ec4a4a26880ddb30895fc10c0afb47182dac020
karthi <karthiseeni96@gmial.com>
2020-08-02 22:57:56 +0530

    Personal files

--- Karthi/Try/My_codes2.py
+++ Karthi/Try/My_codes2.py
@@ -7,3 +7,50 @@
 # a='karthikeyan'
 # print(a[0:11:3])
 # print(a[8:11:2])
+
+# import math
+# print(math.sqrt(4))
+
+# d= {1:(200,34),2:[23,24],3:{34,56}}
+# print(d)
+# 
+# s = r'fgtr\nrtyy'
+# print(s)
+# r='fgtr\nrtyy'
+# print(r)
+
+# a=int(input('Enter the first value: '))
+# b=int(input('Enter the second value: '))
+# c=a*b
+# print(f'Product of {a} and {b} is {c}')
+
+# a=float(input('Enter a list:'))
+# print(a,type(a))
+
+# print(2*2)
+# print(4//2)
+# import math
+# s=math.ceil(67.56)
+# d=math.floor(67.56)
+# print (d)
+# print (s)
+
+# print(~True)
+# print(int(0b10000))
+# s= "My name is karthi"
+# print(s[-1:-7])
+
+# print(dir(int))
+# 
+# print ((10<20)and(5!=100))
+# print ((10<20)and(5==100))
+# print ((10<20)or(5!=100))
+# print((not True))
+# print (10and 20)
+
+print(10&20)
+print(10|20)
+print(10^20)
+print(~10)
+print(10>>2)
+print(10<<2)
