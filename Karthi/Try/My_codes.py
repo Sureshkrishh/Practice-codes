84422405ae56108e50e4518d1471df8d9c4c6d52
karthi <karthiseeni96@gmial.com>
2020-08-03 23:00:35 +0530

    personal files

--- Karthi/Try/My_codes.py
+++
@@ -1,8 +0,0 @@
-# a= int(input('Enter first one: ')) #check with string input here
-# b=int(input('Enter second value: ')) #check with int/float value here
-# c=a+b
-# print("Output is ",c) #find the output and let me know
-
-# a=''''This \nis \npython \nclass'''
-# print(a)
-
--- Karthi/try1.py
+++
@@ -1,4 +0,0 @@
-a= ('Enter first one')
-b=('Enter second value')
-c=a+b
-print(c)
